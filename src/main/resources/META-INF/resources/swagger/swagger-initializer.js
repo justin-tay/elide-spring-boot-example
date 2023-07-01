6152c2dd0c3d3696eb2bef021ef1de66ca110216
Justin Tay <49700559+justin-tay@users.noreply.github.com>
2023-07-01 21:29:13 +0800

    Update swagger

--- src/main/resources/META-INF/resources/swagger/swagger-initializer.js
+++ src/main/resources/META-INF/resources/swagger/swagger-initializer.js
@@ -6,9 +6,21 @@
 window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
 
+  let search;
+  if(parent.window) {
+    search = parent.window.location.search;
+  }
+  if(!search) {
+    search = window.location.search;
+  }
+  let path = new URLSearchParams(search).get('path');
+  if(!path) {
+    path = '';
+  }
+
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "../doc",
+    url: `../doc${path}${search}`,
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
