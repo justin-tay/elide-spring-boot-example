7a42f4e65f07ecacd8caa9d4fcd182cf23017f04
Justin Tay <49700559+justin-tay@users.noreply.github.com>
2024-07-04 17:23:51 +0800

    Configure csrf

--- src/main/java/example/config/SecurityConfigProperties.java
+++ src/main/java/example/config/SecurityConfigProperties.java
@@ -10,8 +10,19 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import lombok.Data;    
 
+/**
+ * Security configuration properties.
+ */
 @Data    
 @ConfigurationProperties(prefix = "app.security")
 public class SecurityConfigProperties {
+    /**
+     * Whether to enable security.
+     */
+    private boolean enabled = false;
+
+    /**
+     * A list of origins for which cross-origin requests are allowed.
+     */
     private String origin = "*";
 }
