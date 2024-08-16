4143cc987c38bd40f86bbd08b2fe5f9b47a0d5e9
Justin Tay <49700559+justin-tay@users.noreply.github.com>
2024-08-16 12:57:27 +0800

    Update

--- src/main/java/example/security/check/NoteCreatorIsUserCheck.java
+++ src/main/java/example/security/check/NoteCreatorIsUserCheck.java
@@ -3,6 +3,9 @@ package example.security.check;
 import java.util.Collections;
 import java.util.List;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
 import com.yahoo.elide.annotation.SecurityCheck;
 import com.yahoo.elide.core.Path;
 import com.yahoo.elide.core.filter.Operator;
@@ -15,6 +18,7 @@ import com.yahoo.elide.core.type.Type;
 import example.config.AppSecurityProperties;
 import example.model.AppUser;
 import example.model.Note;
+import example.security.AppUserGrantedAuthority;
 
 /**
  * {@link FilterExpressionCheck} that the Note createdBy is the user.
@@ -32,12 +36,18 @@ public class NoteCreatorIsUserCheck extends FilterExpressionCheck<Note> {
     @Override
     public FilterExpression getFilterExpression(Type<?> entityClass, RequestScope requestScope) {
         if (this.appSecurityProperties.isEnabled()) {
-            String username = requestScope.getUser().getName();
+            Authentication authentication = requestScope.getUser().getPrincipal();
+            Long id = null;
+            for (GrantedAuthority grantedAuthority : authentication.getAuthorities()) {
+                if (grantedAuthority instanceof AppUserGrantedAuthority appUserGrantedAuthority) {
+                    id = appUserGrantedAuthority.getId();
+                }
+            }
             Path.PathElement notePath = new Path.PathElement(Note.class, AppUser.class, "createdBy");
-            Path.PathElement userPath = new Path.PathElement(AppUser.class, String.class, "username");
+            Path.PathElement userPath = new Path.PathElement(AppUser.class, Long.class, "id");
             List<Path.PathElement> pathList = List.of(notePath, userPath);
             Path path = new Path(pathList);
-            return new FilterPredicate(path, Operator.IN, List.of(username));
+            return new FilterPredicate(path, Operator.IN, List.of(id));
         } else {
             Path.PathElement notePath = new Path.PathElement(Note.class, String.class, "title");
             Path path = new Path(List.of(notePath));
